77bf6e9f799f5e44814007d38016c29cd0abbb79
shivangi2110 <shivangisisodia2003@gmail.com>
2025-07-14 11:25:43 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("NAME : Shivangi Sisodia");
+        System.out.println("ROLL NO : 2210990828");
+        System.out.println("BRANCH : CSE");
+        System.out.println("UNIVERSITY NAME: Chitkara University");
+
+    }
+}
